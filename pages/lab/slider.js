e720ad664c5218376c0c2375b89cc0e02b06b071
Evgeniy Podivilov <evgeniy.podivilov@gmail.com>
2018-03-19 22:41:12 +0500

    Moved pages/slider to pages/lab/slider

--- pages/lab/slider.js
+++ pages/lab/slider.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import withRoot from 'docs/src/modules/components/withRoot';
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import markdown from 'docs/src/pages/demos/slider/slider.md';
+import markdown from 'docs/src/pages/lab/slider/slider.md';
 
 function Page() {
   return (
@@ -9,38 +9,38 @@ function Page() {
       markdown={markdown}
       demos={{
         'pages/demos/slider/SimpleSlider.js': {
-          js: require('docs/src/pages/demos/slider/SimpleSlider').default,
+          js: require('docs/src/pages/lab/slider/SimpleSlider').default,
           raw: preval`
 module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/slider/SimpleSlider'), 'utf8')
+  .readFileSync(require.resolve('docs/src/pages/lab/slider/SimpleSlider'), 'utf8')
 `,
         },
         'pages/demos/slider/StepSlider.js': {
-          js: require('docs/src/pages/demos/slider/StepSlider').default,
+          js: require('docs/src/pages/lab/slider/StepSlider').default,
           raw: preval`
 module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/slider/StepSlider'), 'utf8')
+  .readFileSync(require.resolve('docs/src/pages/lab/slider/StepSlider'), 'utf8')
 `,
         },
         'pages/demos/slider/DisabledSlider.js': {
-          js: require('docs/src/pages/demos/slider/DisabledSlider').default,
+          js: require('docs/src/pages/lab/slider/DisabledSlider').default,
           raw: preval`
 module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/slider/DisabledSlider'), 'utf8')
+  .readFileSync(require.resolve('docs/src/pages/lab/slider/DisabledSlider'), 'utf8')
 `,
         },
         'pages/demos/slider/VerticalSlider.js': {
-          js: require('docs/src/pages/demos/slider/VerticalSlider').default,
+          js: require('docs/src/pages/lab/slider/VerticalSlider').default,
           raw: preval`
 module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/slider/VerticalSlider'), 'utf8')
+  .readFileSync(require.resolve('docs/src/pages/lab/slider/VerticalSlider'), 'utf8')
 `,
         },
         'pages/demos/slider/ReverseSlider.js': {
-          js: require('docs/src/pages/demos/slider/ReverseSlider').default,
+          js: require('docs/src/pages/lab/slider/ReverseSlider').default,
           raw: preval`
 module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/slider/ReverseSlider'), 'utf8')
+  .readFileSync(require.resolve('docs/src/pages/lab/slider/ReverseSlider'), 'utf8')
 `,
         },
       }}
